b52a9280e626c1aca110878efe0876241f8ad350
KIMYOUNGWOON <youngwn2130@gmail.com>
2023-04-21 18:07:03 +0900

    Add: 초기세팅 완료

---
+++ src/Router.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import Main from './pages/Main/Main';
+import ProductDetail from './pages/ProductDetail/ProductDetail';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/detail" element={<ProductDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
