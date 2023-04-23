4f71df7d866723a0952f640671342fddb1ca617a
suny0ung <121158293+suny0ung@users.noreply.github.com>
2023-04-23 22:49:06 +0900

    Add: main page - 모달창 완료

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,217 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import MainHeader from './components/MainHeader';
+import MainImg from './components/MainImg';
+import CategoryBtn from './components/CategoryBtn';
+import FilterBar from './components/FilterBar';
+import Product from './components/Product';
+import styled from 'styled-components';
 
 function Main() {
-  return <div />;
+  const [mainImage, setMainImage] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [offset, setOffset] = useState(0);
+
+  //TODO : 통신 안 할때 쓸 moc데이터 입니다.
+  // const [moc, setMoc] = useState([]);
+  // useEffect(() => {
+  //   fetch('/data/mainMockData.json')
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       setMoc(data);
+  //     });
+  // }, []);
+
+  useEffect(() => {
+    fetch('/data/mainImg.json')
+      .then(res => res.json())
+      .then(data => {
+        setMainImage(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch(`http://10.58.52.65:3000/products?limit=10&offset=${offset}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+    })
+      .then(response => response.json())
+      .then(item => {
+        setProducts(prev => prev.concat(item));
+      });
+    window.addEventListener('scroll', handleScroll);
+  }, [offset]);
+
+  const handleScroll = e => {
+    const num = e.target.documentElement.scrollTop;
+    if (
+      window.innerHeight + num >=
+      e.target.documentElement.scrollHeight - 200
+    ) {
+      setOffset(offset + 10);
+    }
+  };
+
+  return (
+    <MainWrap>
+      <MainHeader />
+      <MainImg mainImage={mainImage} id={mainImage.id} url={mainImage.url} />
+
+      <CategoryMapDiv>
+        {CATEGORY.map(({ id, text }) => {
+          return <CategoryBtn key={id} id={id} text={text} />;
+        })}
+      </CategoryMapDiv>
+
+      <MainContentsWrap>
+        <FilterBar />
+        <Contents>
+          <MainContentsTop>
+            <ItemNum>상품 10,000</ItemNum>
+            <ItemSelect name="">
+              {RIGHT_CATEGORY.map(({ id, text }) => {
+                return (
+                  <ItemOption key={id} name={id} value={text}>
+                    {text}
+                  </ItemOption>
+                );
+              })}
+            </ItemSelect>
+          </MainContentsTop>
+          <ProductsMapDiv>
+            {products?.map(
+              ({
+                productId,
+                productImage,
+                productName,
+                productOriginalPrice,
+                productModelNumber,
+                premiumPercent,
+                likeCount,
+                reviewCount,
+              }) => {
+                return (
+                  <Product
+                    key={productId}
+                    id={productId}
+                    url={productImage}
+                    title={productName}
+                    price={productOriginalPrice}
+                    productNum={productModelNumber}
+                    likeCount={likeCount}
+                    reviewCount={reviewCount}
+                    percent={Math.floor(premiumPercent).toFixed(2)}
+                  />
+                );
+              }
+            )}
+          </ProductsMapDiv>
+        </Contents>
+      </MainContentsWrap>
+    </MainWrap>
+  );
 }
 
+const MainWrap = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const CategoryMapDiv = styled.div`
+  width: 100vw;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 3px;
+  margin: 0 auto;
+  padding: 40px 0 85px 0;
+  border-bottom: 1px solid rgba(188, 188, 188, 0.5);
+  align-items: center;
+`;
+
+const MainContentsWrap = styled.div`
+  display: flex;
+  justify-content: baseline;
+  width: 90vw;
+  height: 100vh;
+`;
+
+const Contents = styled.div`
+  width: 75vw;
+  padding: 10px;
+`;
+
+const MainContentsTop = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 23px 0;
+  padding: 5px 10px;
+  align-items: center;
+`;
+
+const ItemNum = styled.p`
+  font-size: 17px;
+`;
+const ItemSelect = styled.select`
+  width: 160px;
+  padding: 5px 10px;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid rgba(188, 188, 188, 0.2);
+  color: gray;
+  appearance: none;
+  font-size: 15px;
+
+  &:hover {
+    border: 1px solid rgba(188, 188, 188, 0.5);
+    color: black;
+  }
+`;
+
+const ItemOption = styled.option`
+  font-size: 15px;
+  text-align: center;
+`;
+
+const ProductsMapDiv = styled(MainWrap)`
+  display: flex;
+  justify-items: center;
+  flex-wrap: wrap;
+  padding-left: 80px;
+`;
+
 export default Main;
+
+const CATEGORY = [
+  { id: 1, text: 'POINT QUIZ' },
+  { id: 2, text: 'BEST' },
+  { id: 3, text: 'NEW' },
+  { id: 4, text: 'EVENT' },
+];
+
+const RIGHT_CATEGORY = [
+  { id: 0, text: '선택', endpoint: '' },
+  { id: 1, text: '좋아요 순', endpoint: 'like' },
+  { id: 2, text: '리뷰순', endpoint: 'review' },
+  {
+    id: 3,
+    text: '즉시판매가',
+    endpoint: '&sort=immediatesellprice&sortorder=asc',
+  },
+  {
+    id: 4,
+    text: '즉시구매가',
+    endpoint: '&sort=immediatebuyprice&sortorder=desc',
+  },
+  {
+    id: 5,
+    text: '프리미엄 가격 높은 순',
+    endpoint: '&sort=premium&sortorder=desc',
+  },
+  {
+    id: 6,
+    text: '프리미엄 가격 낮은 순',
+    endpoint: '&sort=premium&sortorder=asc',
+  },
+];
---
+++ src/pages/Main/components/Product.js
@@ -0,0 +1,182 @@
+import React from 'react';
+import { CiBookmark, CiMemoPad } from 'react-icons/ci';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+function Product({
+  id,
+  url,
+  categoryName,
+  title,
+  price,
+  productNum,
+  percent,
+  likeCount,
+  reviewCount,
+  productOriginalPrice,
+}) {
+  const navigate = useNavigate();
+  const goToDetail = () => {
+    navigate('/detail');
+  };
+  const goToInterest = () => {
+    navigate('/interestProduct');
+  };
+  return (
+    <ProductsBox>
+      <ProductWrap>
+        <TopItemWrap onClick={goToDetail}>
+          <ImgWrap>
+            <PremiumValue>{percent}%</PremiumValue>
+            <Img src={url} alt="img" />
+          </ImgWrap>
+
+          <TextWrap>
+            <Title>{title}</Title>
+            <ProductNum> No. {productNum}</ProductNum>
+            <Price>{Math.floor(price).toLocaleString()}</Price>
+            <OriginalPrice>{productOriginalPrice}</OriginalPrice>
+          </TextWrap>
+        </TopItemWrap>
+
+        <ButtonWrap>
+          <BookmarkWrap>
+            <CiBookmark />
+            <LikeCount onClick={goToInterest}>{likeCount}</LikeCount>
+          </BookmarkWrap>
+          <ReviewWrap>
+            <CiMemoPad />
+            <ReviewCount>{reviewCount}</ReviewCount>
+          </ReviewWrap>
+        </ButtonWrap>
+      </ProductWrap>
+    </ProductsBox>
+  );
+}
+
+const ProductsBox = styled.div`
+  display: flex;
+  justify-content: center;
+  flex: 1 1 270px;
+  cursor: pointer;
+`;
+
+const ProductWrap = styled.div`
+  width: 270px;
+  height: 430px;
+  padding: 10px;
+`;
+
+const TopItemWrap = styled.div``;
+
+const ImgWrap = styled.div`
+  position: relative;
+`;
+
+const PremiumValue = styled.div`
+  position: absolute;
+  top: 13px;
+  right: 10px;
+  font-size: 13px;
+  font-weight: 700;
+  color: red;
+`;
+
+const TextWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 0 7px 0;
+  padding: 0 10px;
+  font-size: 14px;
+  line-height: 16px;
+`;
+
+const Title = styled.p`
+  margin: 10px 0px 0px;
+  font-size: 17px;
+  word-break: keep-all;
+  line-height: 2em;
+  font-weight: 700;
+`;
+
+const ProductNum = styled.p`
+  display: flex;
+  align-items: center;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  margin-bottom: 6px;
+  color: #777;
+  /* line-height: 16px;
+  font-size: 13px;
+  letter-spacing: -0.07px;
+  font-weight: 700;
+  color: #333; */
+`;
+
+const Price = styled.p`
+  display: flex;
+  align-items: center;
+  /* height: 30px;
+  line-height: 17px;
+  font-size: 14px;
+  font-weight: 700; */
+  font-size: 17px;
+  font-weight: 700;
+  color: #333;
+  margin-right: 4px;
+  margin: 10px 0 8px 0;
+  /* padding: 5px 10px; */
+`;
+
+const OriginalPrice = styled.p``;
+
+const ButtonWrap = styled.div`
+  display: flex;
+  /* justify-content: space-between; */
+`;
+
+const BookmarkWrap = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  margin-left: 4px;
+  color: rgba(34, 34, 34, 0.5);
+  font-size: 20px;
+`;
+
+const LikeCount = styled.p`
+  display: flex;
+  align-items: center;
+  padding: 0 5px 0 3px;
+  font-size: 12px;
+`;
+
+const ReviewCount = styled.p`
+  display: flex;
+  align-items: center;
+  padding: 0 0 0 5px;
+  font-size: 12px;
+`;
+
+const ReviewWrap = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  margin-left: 4px;
+  color: rgba(34, 34, 34, 0.5);
+  font-size: 20px;
+`;
+const Img = styled.img`
+  width: 250px;
+  height: 250px;
+  padding: 10px;
+  object-fit: contain;
+  object-position: center;
+  border: 2px solid rgba(188, 188, 188, 0.2);
+  border-radius: 5px;
+
+  &:hover {
+    border: 1px solid rgb(188, 188, 188);
+  }
+`;
+
+export default Product;
