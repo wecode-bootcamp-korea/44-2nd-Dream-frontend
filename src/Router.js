bc7c0463948ca0a6f533ecc575b501c446b4ae2b
KIMYOUNGWOON <youngwn2130@gmail.com>
2023-04-24 19:28:37 +0900

    Add: 구매(판매) 입찰페이지 수정사항 반영완료

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AgreePage from './pages/AgreePage/AgreePage';
+import BiddingPage from './pages/BiddingPage/BiddingPage';
 import InterestProduct from './pages/InterestProduct/InterestProduct';
 import Login from './pages/Login/Login';
 import Token from './pages/Login/Token';
@@ -8,7 +9,6 @@ import Main from './pages/Main/Main';
 import Payment from './pages/Payment/Payment.js';
 import SearchModal from './pages/Main/components/SearchModal';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
-import PurchaseBid from './pages/PurchaseBid/PurchaseBid';
 
 const Router = () => {
   return (
@@ -16,13 +16,12 @@ const Router = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/dream/kakao" element={<Token />} />
-
         <Route path="/" element={<Main />} />
         <Route path="/modal" element={<SearchModal />} />
         <Route path="/detail" element={<ProductDetail />} />
         <Route path="/interestProduct" element={<InterestProduct />} />
         <Route path="/agree" element={<AgreePage />} />
-        <Route path="/purchaseBid" element={<PurchaseBid />} />
+        <Route path="/bidding" element={<BiddingPage />} />
         <Route path="/payment" element={<Payment />} />
       </Routes>
     </BrowserRouter>
