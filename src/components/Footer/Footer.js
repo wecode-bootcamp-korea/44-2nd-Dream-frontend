369efe3f50bdedf709043244f7e0e56f5b75e3ab
suny0ung <121158293+suny0ung@users.noreply.github.com>
2023-04-25 21:42:33 +0900

    Add: Footer 레이아웃 완료

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function Footer() {
-  return <div />;
+  return (
+    <FooterBox>
+      <div>
+        <Logo>DREAM</Logo>
+        <OurTeam> FE: Kimyoungoun / Choisunyoung / Chogunho </OurTeam>
+        <OurTeam> BE: Kimminsu / Jangdahee / Barkseayick / Songsukjun </OurTeam>
+        <InWecode>© WECODE DREAM 2023</InWecode>
+      </div>
+    </FooterBox>
+  );
 }
+
+const FooterBox = styled.div`
+  width: 100vw;
+  height: 13vh;
+  div {
+    padding: 8px 0;
+  }
+`;
+
+const Logo = styled.h1`
+  display: flex;
+  justify-content: center;
+  padding: 10px 0;
+  font-size: 30px;
+  font-weight: 900;
+  font-style: italic;
+`;
+
+const OurTeam = styled.p`
+  display: flex;
+  justify-content: center;
+  font-size: 16px;
+`;
+
+const InWecode = styled.p`
+  display: flex;
+  padding: 10px 0;
+  justify-content: center;
+  font-size: 16px;
+`;
 
 export default Footer;
